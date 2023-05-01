6af01dcdb312286ef4e301075cd31a6f5e26e783
romanconstantin1 <romanconstantin1@gmail.com>
2023-05-01 09:56:13 +0000

    test passed, need to add 400 & 404s

--- src/app.py
+++ src/app.py
@@ -30,11 +30,41 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    response_body = members
 
+    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_family_member(id):
+    found_member = jackson_family.get_member(id)
+    response_body = {
+        "first_name": found_member[0]["first_name"],
+        "id": found_member[0]["id"],
+        "age": found_member[0]["age"],
+        "lucky_numbers": found_member[0]["lucky_numbers"]
+    }
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_family_member():
+    new_request = request.json
+    new_member = jackson_family.add_member(new_request)
+    response = {
+        "msg": "New family member added successfully",
+        "New family member": f'New family member: {new_member["first_name"]}'
+    }
+    return jsonify(response), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def remove_family_member(id):
+    deleted_member = jackson_family.delete_member(id)
+    response_body = {
+        "first_name": deleted_member["first_name"],
+        "id": deleted_member["id"],
+        "age": deleted_member["age"],
+        "lucky_numbers": deleted_member["lucky_numbers"],
+        "done": deleted_member["done"]
+    }
 
     return jsonify(response_body), 200
 
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,24 +13,65 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
+    def get_member(self, id):
+        print(id)
+        member = list(filter(lambda sub: sub["id"] in [id], self._members))
+        return member
+
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        new_member = {
+            "id": member["id"],
+            "first_name": member["first_name"],
+            "last_name": self.last_name,
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"]
+        }
+        self._members.append(new_member)
+        return new_member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for i in range(len(self._members)):
+            if self._members[i]["id"] == id:
+                to_delete = self._members[i]
+                del self._members[i]
+                to_delete["done"] = True
+                print(to_delete)
+                break
+        # to_delete = list(filter(lambda sub: sub['id'] != id, self._members))
+        # print(to_delete)
+        # to_delete[0]["done"] = True
+        print(self._members)
+        return to_delete
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
+        # print(self._members)
         return self._members
